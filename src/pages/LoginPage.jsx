79e2de9bbd3d92ff8b37d8586f4bad60559aa480
StathisKotanidis <stathiskotanidhs@gmail.com>
2024-12-11 21:11:42 +0200

    homepage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,6 +2,10 @@ function LoginPage() {
   return (
     <div>
       <div>Welcome back, #NAME</div>
+      <form>
+        <label htmlFor=""></label>
+        <input></input>
+      </form>
     </div>
   );
 }
