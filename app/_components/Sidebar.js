fe48eb8f6f1c706d1837a1fbc2c25edc93a9e30f
StathisKotanidis <stathiskotanidhs@gmail.com>
2025-01-13 23:00:42 +0200

    implemented the navigation for my pages

--- app/_components/Sidebar.js
+++ app/_components/Sidebar.js
@@ -8,9 +8,16 @@ import { FaBars } from "react-icons/fa";
 import { IoCloseSharp } from "react-icons/io5";
 import React from "react";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Sidebar() {
   const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
+
+  function handleNavigation(page) {
+    router.push(`/${page}`);
+  }
+
   return (
     <div
       className={`h-screen w-20 md:w-64 bg-mainColor transition-width duration-300 text-white ${
@@ -31,25 +38,37 @@ export default function Sidebar() {
       </div>
       <nav className="mt-4">
         <ul>
-          <li className="flex items-center p-4 hover:bg-green-800 cursor-pointer">
+          <li
+            onClick={() => handleNavigation("profile")}
+            className="flex items-center p-4 hover:bg-gray-600 cursor-pointer"
+          >
             <CgProfile size={24} />
             <span className={`ml-4 md:block ${isOpen ? "block" : "hidden"}`}>
               Profile
             </span>
           </li>
-          <li className="flex items-center p-4 hover:bg-gray-700 cursor-pointer">
+          <li
+            onClick={() => handleNavigation("favorites")}
+            className="flex items-center p-4 hover:bg-gray-600 cursor-pointer"
+          >
             <GrFavorite size={24} />
             <span className={`ml-4 md:block ${isOpen ? "block" : "hidden"}`}>
               Favorites
             </span>
           </li>
-          <li className="flex items-center p-4 hover:bg-gray-700 cursor-pointer">
+          <li
+            onClick={() => handleNavigation("history")}
+            className="flex items-center p-4 hover:bg-gray-600 cursor-pointer"
+          >
             <AiOutlineHistory size={24} />
             <span className={`ml-4 md:block ${isOpen ? "block" : "hidden"}`}>
               History
             </span>
           </li>
-          <li className="flex items-center p-4 hover:bg-gray-700 cursor-pointer">
+          <li
+            onClick={() => handleNavigation("login")}
+            className="flex items-center p-4 hover:bg-gray-600 cursor-pointer"
+          >
             <IoMdPower size={24} />
             <span className={`ml-4 md:block ${isOpen ? "block" : "hidden"}`}>
               Log Out
